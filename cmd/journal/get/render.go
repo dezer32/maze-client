933218fbaaf24666fb8f52b5fbe57ed2a6c3eb75
Dezer <my@ipro.dev>
2023-04-06 23:30:45 +0300

    fix view

--- cmd/journal/get/render.go
+++ cmd/journal/get/render.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"sort"
 	"strconv"
-	"time"
 
 	"github.com/olekukonko/tablewriter"
 
@@ -25,37 +24,38 @@ func Render(journals []*client.Journal, userId int) *tablewriter.Table {
 	// tw.SetAutoWrapText(true)
 	tw.SetAutoMergeCells(true)
 
-	tw.SetHeader([]string{"date", "ticket", "title", "comment"})
+	tw.SetHeader([]string{"date", "ticket", "title" /*, "comment"*/})
 	tw.SetHeaderColor(
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor},
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor},
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiCyanColor},
-		tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor},
+		// tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor},
 	)
 
 	tw.SetColumnColor(
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor},
 		tablewriter.Colors{tablewriter.FgHiRedColor},
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiCyanColor},
-		tablewriter.Colors{tablewriter.FgCyanColor},
+		// tablewriter.Colors{tablewriter.FgCyanColor},
 	)
 
-	var prevDate time.Time
+	// var prevDate time.Time
 	for _, v := range journals {
 		if v.User != userId {
 			continue
 		}
 
-		date := ""
-		if !prevDate.Equal(v.Date.Time) {
-			date = v.Date.Format("02.01.2006")
-		}
+		// date := ""
+		// if !prevDate.Equal(v.Date.Time) {
+		// 	prevDate = v.Date.Time
+		// 	date = v.Date.Format("02.01.2006")
+		// }
 
 		tw.Append([]string{
-			date,
+			v.Date.Format("02.01.2006"),
 			strconv.Itoa(v.Ticket),
 			v.TicketTitle,
-			v.Comment,
+			// v.Comment,
 		})
 	}
 
